4a8e186bcfd33596b17c526ef2455331b0c4ae01
Martin Badin <martin.badin@gmail.com>
2019-01-10 09:00:43 +0000

    Merged in PBA-1057-attachment-placeholder (pull request #23)
    add placeholder for attachments
    
    Approved-by: Jozef Pochyba <jozef.pochyba@thinkcreatix.com>

--- src/Components/MessageAttachments.js
+++ src/Components/MessageAttachments.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as React from 'react';
+import { View, StyleSheet } from 'react-primitives';
 
 type Props = {
   context: *,
@@ -10,6 +11,20 @@ type Props = {
 type State = {
   attachments: Array<Object>,
 };
+
+function AttachmentPlaceholder() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.image}>
+        <View style={styles.icon} />
+      </View>
+      <View style={styles.wrapper}>
+        <View style={styles.text} />
+        <View style={styles.text} />
+      </View>
+    </View>
+  );
+}
 
 export class MessageAttachments extends React.Component<Props, State> {
   state = {
@@ -26,8 +41,30 @@ export class MessageAttachments extends React.Component<Props, State> {
     this.setState({ attachments });
   }
 
-  render() {
+  renderFiles = () => {
     const { Files } = this.props.context.components;
     return <Files files={this.state.attachments} />;
+  };
+
+  renderPlaceholders = () => {
+    return this.props.attachments.map(attachment => <AttachmentPlaceholder />);
+  };
+
+  render() {
+    return this.state.attachments.length ? this.renderFiles() : this.renderPlaceholders();
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 56,
+    width: 285,
+    paddingTop: 10,
+  },
+  image: { width: 46, height: 56, alignItems: 'center', justifyContent: 'center' },
+  wrapper: { flex: 1, paddingLeft: 15 },
+  text: { height: 14, marginVertical: 2, borderRadius: 3, backgroundColor: '#eee', flexGrow: 1 },
+  icon: { width: 39, height: 50, borderRadius: 3, backgroundColor: '#eee' },
+});
